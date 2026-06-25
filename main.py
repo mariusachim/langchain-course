6ae4e9fe9214c0c41704664bfa09b9bd3977d3c9
machim <mariusachim@gmail.com>
2026-06-25 21:10:51 +0300

    feat: switch to gemma3 by ollama

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from dotenv import load_dotenv
 from langchain_core.prompts import PromptTemplate
 from langchain_openai import ChatOpenAI
+from langchain_ollama import ChatOllama
 
 load_dotenv()
 
@@ -27,7 +28,8 @@ Musk's political activities, statements and views have made him a polarizing fig
         input_variables=["information"], template=summary_template
     )
 
-    llm = ChatOpenAI(temperature=0, model_name="gpt-5")
+    #llm = ChatOpenAI(temperature=0, model_name="gpt-5")
+    llm = ChatOllama(temperature=0, model="gemma3:270m")
     chain = summary_prompt_template | llm
     response = chain.invoke(input={"information":information})
     print(response.content)
